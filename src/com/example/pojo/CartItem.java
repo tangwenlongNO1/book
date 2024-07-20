62082997e1eb74404755de1ae07b00610235bb89
twl <twl102841@gmail.com>
2024-07-20 15:04:15 +0800

    new:购物车模块实现

---
+++ src/com/example/pojo/CartItem.java
@@ -0,0 +1,86 @@
+package com.example.pojo;
+
+import java.math.BigDecimal;
+
+/**
+ * Class name: CartItem
+ * Package name: com.example.pojo
+ * Project name: book
+ *
+ * @Author: Jason Tom
+ * Description:购物车item
+ * @Create_time: 2024/7/20-14:10
+ */
+
+public class CartItem {
+
+    private Integer id;
+    private String name;
+    private Integer count;
+    private BigDecimal price;
+    private BigDecimal totalPrice;
+
+    public CartItem() {
+    }
+
+    public CartItem(Integer id, String name, Integer count, BigDecimal price, BigDecimal totalPrice) {
+        this.id = id;
+        this.name = name;
+        this.count = count;
+        this.price = price;
+        this.totalPrice = totalPrice;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "CartItem{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", count=" + count +
+                ", price=" + price +
+                ", totalPrice=" + totalPrice +
+                '}';
+    }
+}
