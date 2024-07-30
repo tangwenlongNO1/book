d5332641aab16857fe7f01f34eaafee4c56a72ce
twl <twl102841@gmail.com>
2024-07-30 23:32:43 +0800

    feat:添加filter

---
+++ src/com/example/filter/ManagerFilter.java
@@ -0,0 +1,39 @@
+package com.example.filter;
+/**
+ * Class name: ${NAME}
+ * Package name: ${PACKAGE_NAME}
+ * Project name: book
+ *
+ * @Author: Jason Tom
+ * Description:
+ * @Create_time: 2024/7/30-16:24
+ */
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter(filterName = "ManagerFilter", value = {"/pages/manager/*", "/manager/BookServlet"})
+public class ManagerFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        Object user = httpServletRequest.getSession().getAttribute("user");
+        if (user == null) {
+            httpServletRequest.getRequestDispatcher("/pages/user/login.jsp").forward(request, response);
+        } else {
+
+            chain.doFilter(request, response);
+        }
+
+    }
+}
